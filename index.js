a36d2f01fb7f88cb7805fbefff6a74bf8e9b3be9
Elaine Froehlich <elaine.froehlich@gmail.com>
2017-12-10 02:58:09 +0000

    no location data access no matter what I do

--- index.js
+++ index.js
@@ -1,42 +1,91 @@
-/* global $ APIKEY */
+/* global $ APIKEY navigator position  */
 //intermediate local weather api app
 //makes sure the page loads before the javascript or jquery runs
-
-//code example from news api to start
-$(document).ready(function(){
-    $.ajax({
-        method: "GET",
-        //url: "api.openweathermap.org/data/2.5/weather?q={boston},{USA}",
-        //url: "api.openweathermap.org/data/2.5/weather?q={city name},{country code}",
-        url: "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}",
-        //url: "https://api.openweathermap.org/data/2.5/forecast?id={APIKEY}",
-        //is loading the data from the id 52901 which is Moscow. not reading data below
-        data: {
-            "coord": {"lon":145.77,"lat":-16.92},
-            "weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],
-            "base":"cmc stations",
-            "main":{"temp":293.25,"pressure":1019,"humidity":83,"temp_min":289.82,"temp_max":295.37},
-            "wind":{"speed":5.1,"deg":150},
-            "clouds":{"all":75},
-            "rain":{"3h":3},
-            "dt":1435658272,
-            "sys":{"type":1,"id":8166,"message":0.0166,"country":"AU","sunrise":1435610796,"sunset":1435650870},
-            "id":2172797,
-            "name":"Cairns",
-            "cod":200, 
-            "apiKey": APIKEY
-            },
+    $(document).ready(function(){
+       //$("body").css("");
+       console.log("did it")
+    });
+    
+    
+    // var x =document.getElementById("position");
+    //     function getLatLong() {
+    //         if (navigator.geolocation) {
+    //             navigator.geolocation.getCurrentPosition(showPosition);
+    //         } else { 
+    //             x.innerHTML = "Geolocation not supported by this browser";
+                
+    //         }
+    //     };
+    //     console.log("position");
+    //     function showPosition() {
+    //         x.innerHTML = "Latitude = " + position.coords.lattitude + "<br>Longitude = " + position.coords.longitude;
             
-            success: function(data) {
-                //sanity check
-                //if (data.status == "ok") {
-                    console.log(data);
-                //}
-            }, 
+    //     };
+        //console.log(showPosition());
+        var getGeoLoc = {
+        "longitued": "showPosition.position.coords.longiitude",
+        "latitude": "getLatLong.position.coords.latiitude",
+        
+    };
+        // console.log(showPosition.position.coords.lattitud);
+        console.log("getGeoLoc");
+        //ajax call
+        //code example from news api to start
+                // $.ajax({
+                //     method: "GET",
+                //     //url: "api.openweathermap.org/data/2.5/weather?q={boston},{USA}",
+                //     //url: "api.openweathermap.org/data/2.5/weather?q={city name},{country code}",
+                //     url: "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}",
+                //     //url: "https://api.openweathermap.org/data/2.5/forecast?id={APIKEY}",
+                //     //is loading the data from the id 52901 which is Moscow. not reading data below
+                //     data: {
+                //         "coord": {"lon": "showPosition.position.coords.longiitude", "lat": ""},
+                        
+                //         "apiKey": APIKEY    
+                //     }
+                    
+                // });    
+        
+        
+        
+          
+         
+         
+// //code example from news api to start
+// $(document).ready(function(){
+//     $.ajax({
+//         method: "GET",
+//         //url: "api.openweathermap.org/data/2.5/weather?q={boston},{USA}",
+//         //url: "api.openweathermap.org/data/2.5/weather?q={city name},{country code}",
+//         url: "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}",
+//         //url: "https://api.openweathermap.org/data/2.5/forecast?id={APIKEY}",
+//         //is loading the data from the id 52901 which is Moscow. not reading data below
+//         data: {
+//             "coord": {"lon":145.77,"lat":-16.92},
+//             "weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],
+//             "base":"cmc stations",
+//             "main":{"temp":293.25,"pressure":1019,"humidity":83,"temp_min":289.82,"temp_max":295.37},
+//             "wind":{"speed":5.1,"deg":150},
+//             "clouds":{"all":75},
+//             "rain":{"3h":3},
+//             "dt":1435658272,
+//             "sys":{"type":1,"id":8166,"message":0.0166,"country":"AU","sunrise":1435610796,"sunset":1435650870},
+//             "id":2172797,
+//             "name":"Cairns",
+//             "cod":200, 
+//             "apiKey": APIKEY
+//             },
             
-        });
-    }
-);
+//             success: function(data) {
+//                 //sanity check
+//                 //if (data.status == "ok") {
+//                     console.log(data);
+//                 //}
+//             }, 
+            
+//         });
+//     }
+// );
 //above has correct parens and curlies
     
     
@@ -64,3 +113,23 @@ $(document).ready(function(){
 // Weather should display current temperature
 // Current weather section should allow a click to change from Fahrenheit to Celsius. This could be a button or some other method.
 // Weather must be pulled from an API. Recommended API is the Open Weather API however you are free to use any weather API you wish.
+
+
+
+//get position code used above
+       
+//         var x = document.getElementById("position");
+//         function getLocation() {
+//             if (navigator.geolocation) {
+//             navigator.geolocation.getCurrentPosition(showPosition);
+//             } else {
+//                 x.innerHTML = "Geolocation not supported by this browser";
+                
+//             }
+//         }
+//         function showPosition(position) {
+//             x.innerHTML = "Latitude = " + position.coords.latitude + "<br>Longitude = "
+//              + position.coords.longitude;
+             
+//          }
+//          //console.log(showPosition(position));
